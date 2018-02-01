3f04b8928ac32a390d7887ea1a24629a43e2af9d
Siarhei.Tkachenka <Siarhei.Tkachenka@thomsonreuters.com>
2018-02-01 18:34:48 +0300

    version 1

---
+++ ReadFiles/ReadService.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace ReadFiles
+{
+    public class ReadService
+    {
+        public string ReadTextFile(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ ReadFiles/ReaderApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReadFiles
+{
+    public partial class ReaderApp : Form
+    {
+        private readonly ReadService _readService = new ReadService();
+        private string _path;
+        public ReaderApp()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                _path = dialog.FileName;
+                ReadFile(_path);
+
+                textBox1.Text = _path;
+            }
+        }
+
+        private void ReadFile(string path)
+        {
+            try
+            {
+                textBox2.Text = _readService.ReadTextFile(path);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error occured while reading file.Additional info {exception.Message}");
+            }
+
+        }
+    }
+}
